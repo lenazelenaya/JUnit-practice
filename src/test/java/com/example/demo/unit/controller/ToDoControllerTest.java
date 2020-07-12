ffdf09957d0bc459d42ce5cd18768cfcb60bcdd6
lenazelenaya <lena.zelenaya@gmail.com>
2020-07-12 23:10:49 +0300

    Add initial files

---
+++ src/test/java/com/example/demo/unit/controller/ToDoControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.unit.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoControllerTest {
+
+    @Test
+    void handleException() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void testSave() {
+    }
+
+    @Test
+    void getOne() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
