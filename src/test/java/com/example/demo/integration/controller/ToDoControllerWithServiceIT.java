4019ec58594b460b09c4ab5284e6f7adea7fe701
lenazelenaya <lena.zelenaya@gmail.com>
2020-07-13 14:51:53 +0300

    Add some more tests.2

--- src/test/java/com/example/demo/integration/controller/ToDoControllerWithServiceIT.java
+++ src/test/java/com/example/demo/integration/controller/ToDoControllerWithServiceIT.java
@@ -3,7 +3,11 @@ package com.example.demo.integration.controller;
 import com.example.demo.controller.ToDoController;
 import com.example.demo.dto.mapper.ToDoEntityToRequestMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.util.Collections;
+import java.util.Optional;
 
 import com.example.demo.model.ToDoEntity;
 import com.example.demo.repository.ToDoRepository;
@@ -61,7 +66,6 @@ class ToDoControllerWithServiceIT {
 			.andExpect(jsonPath("$[0].completedAt").doesNotExist());
 	}
 
-	//Does not work
 	@Test
 	void whenSave_thenReturnValidResponse() throws Exception {
 		String testText = "My to do text";
@@ -69,12 +73,13 @@ class ToDoControllerWithServiceIT {
 		var newEntity = new ToDoEntity(testText);
 		var newRequest = ToDoEntityToRequestMapper.map(newEntity);
 
-		when(toDoRepository.save(newEntity)).thenReturn(new ToDoEntity(testId, testText));
+		when(toDoRepository.save(ArgumentMatchers.any(ToDoEntity.class))).thenReturn(new ToDoEntity(testId, testText));
 
 		this.mockMvc
 				.perform(post("/todos")
 						.contentType(MediaType.APPLICATION_JSON)
-						.content(objectMapper.writeValueAsString(newRequest)))
+						.content(objectMapper.writeValueAsString(newRequest))
+						.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("$").isNotEmpty())
@@ -83,21 +88,47 @@ class ToDoControllerWithServiceIT {
 				.andExpect(jsonPath("$.completedAt").doesNotExist());
 	}
 
-	//Does not work
 	@Test
 	void whenGetText_thenReturnValidResponse() throws Exception {
 		Long testId = 1L;
 		String testText = "Test text";
 
-		when(toDoRepository.getOne(testId)).thenReturn(new ToDoEntity(testId, testText));
+		when(toDoRepository.findById(testId)).thenReturn(Optional.of(new ToDoEntity(testId, testText)));
 
 		this.mockMvc
-				.perform(get("/todos/{id}", testId))
+				.perform(get("/todos/{id}/text", testId))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("text/plain;charset=UTF-8"))
+				.andExpect(jsonPath("$").isNotEmpty())
+				.andExpect(jsonPath("$").isString())
+				.andExpect(jsonPath("$").value(testText))
+				.andExpect(jsonPath("$.completedAt").doesNotExist());
+	}
+
+	@Test
+	void whenUpdate_thenReturnValidResponse() throws Exception {
+		String testText = "My to do text";
+		String newText = "New text";
+		Long testId = 1L;
+		var testEntity = new ToDoEntity(testId, testText);
+		var newEntity =  new ToDoEntity(testId, newText);
+		var newRequest = ToDoEntityToRequestMapper.map(testEntity);
+
+		when(toDoRepository.findById(testId)).thenReturn(Optional.of(testEntity));
+		when(toDoRepository.save(ArgumentMatchers.any(ToDoEntity.class)))
+				.thenReturn(newEntity);
+
+		this.mockMvc
+				.perform(post("/todos")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(newRequest))
+						.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("$").isNotEmpty())
-				.andExpect(jsonPath("$.text").value(testText))
+				.andExpect(jsonPath("$.text").value(newText))
 				.andExpect(jsonPath("$.id").value(testId))
 				.andExpect(jsonPath("$.completedAt").doesNotExist());
 	}
+
 }
